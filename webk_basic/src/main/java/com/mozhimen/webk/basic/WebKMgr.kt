e065afc54ca6ca2c4a11bbc08d439de8de9f43b7
Kolin Zhao & Mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-02-28 19:22:11 +0800

    0228

--- webk_basic/src/main/java/com/mozhimen/webk/basic/WebKMgr.kt
+++ webk_basic/src/main/java/com/mozhimen/webk/basic/WebKMgr.kt
@@ -14,6 +14,8 @@ import com.mozhimen.basick.utilk.android.os.UtilKBuildVersion
  * @Version 1.0
  */
 object WebKMgr {
+    private var _isInitWeb = false
+
     /**
      * 如果您的应用必须在多个进程中使用 WebView实例，则您必须先使用 WebView.setDataDirectorySuffix() 方法为每个进程指定唯一的数据目录后缀，然后再在相应进程中使用 WebView 的给定实例。该方法会将每个进程的网络数据放入应用数据目录内其自己的目录中。
      * 在进程中的任何WebView实例被创建之前，并且在此进程中调用android.webkit包中的任何其他方法之前，必须先调用WebView.setDataDirectorySuffix()设置进程的数据目录。
@@ -22,7 +24,8 @@ object WebKMgr {
     @OApiInit_InApplication
     fun init(context: Context) {
         //Android 9 or above must be set
-        if (UtilKBuildVersion.isAfterV_28_9_P()) {
+        if (UtilKBuildVersion.isAfterV_28_9_P() && !_isInitWeb) {
+            _isInitWeb = true
             val processName = Application.getProcessName()
             if (context.packageName != processName) {
                 WebView.setDataDirectorySuffix(processName)
