428a325fc163c33e264487c32b7fffced09c5c7c
Kolin Zhao & Mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-04-01 21:15:51 +0800

    0401

---
+++ webk_browser/src/main/java/com/mozhimen/webk/browser/utils/CustomTabsIntentUtil.kt
@@ -0,0 +1,40 @@
+package com.mozhimen.webk.browser.utils
+
+import android.content.Context
+import android.net.Uri
+import androidx.browser.customtabs.CustomTabsIntent
+import com.mozhimen.basick.elemk.commons.IExtension_Listener
+
+/**
+ * @ClassName CustomTabsIntentUtil
+ * @Description TODO
+ * @Author Mozhimen & Kolin Zhao
+ * @Date 2024/4/1
+ * @Version 1.0
+ */
+object CustomTabsIntentUtil {
+    @JvmStatic
+    fun launchUrl(context: Context, uri: Uri) {
+        CustomTabsIntent.Builder().build().launchUrl(context, uri)
+    }
+
+    @JvmStatic
+    fun launchUrl(context: Context, uri: Uri, intentBuilderBlock: IExtension_Listener<CustomTabsIntent.Builder>) {
+        CustomTabsIntent.Builder().apply { this.intentBuilderBlock() }.build().launchUrl(context, uri)
+    }
+
+//    //设置颜色方案
+//    @JvmStatic
+//    fun launchUrl(context: Context, uri: Uri, block: IExtension_Listener<CustomTabColorSchemeParams.Builder>) {
+//        val schemeParams = CustomTabColorSchemeParams.Builder()
+//            .apply {
+//                //.setToolbarColor(ContextCompat.getColor(context, R.color.colorPrimary))
+//                //.setSecondaryToolbarColor(ContextCompat.getColor(context, R.color.colorPrimaryDark))
+//                this.block()
+//            }
+//            .build()
+//        CustomTabsIntent.Builder()
+//            .setDefaultColorSchemeParams(schemeParams)
+//            .build().launchUrl(context, uri)
+//    }
+}
---
+++ webk_browser_test/src/main/java/com/mozhimen/webk/browser/test/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.mozhimen.webk.browser.test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mozhimen.webk.browser.utils.CustomTabsIntentUtil
+import com.mozhimen.basick.utilk.kotlin.strUrl2uri
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        CustomTabsIntentUtil.launchUrl(this,"https://www.baidu.com".strUrl2uri())
+    }
+}
