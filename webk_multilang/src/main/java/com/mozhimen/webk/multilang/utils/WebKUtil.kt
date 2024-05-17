d3aacd7794e3bec2291c39a16a7b673688548a9f
Kolin Zhao & Mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-05-17 21:35:58 +0800

    0517

--- webk_multilang/src/main/java/com/mozhimen/webk/multilang/utils/WebKUtil.kt
+++ webk_multilang/src/main/java/com/mozhimen/webk/multilang/utils/WebKUtil.kt
@@ -2,6 +2,8 @@ package com.mozhimen.webk.multilang.utils
 
 import android.content.Context
 import com.mozhimen.basick.utilk.android.content.startContext
+import com.mozhimen.basick.utilk.android.util.UtilKLogWrapper
+import com.mozhimen.basick.utilk.commons.IUtilK
 import com.mozhimen.webk.multilang.bases.BaseWebKMultiLangActivity
 
 /**
@@ -11,7 +13,7 @@ import com.mozhimen.webk.multilang.bases.BaseWebKMultiLangActivity
  * @Date 2023/12/24 18:32
  * @Version 1.0
  */
-object WebKUtil {
+object WebKUtil : IUtilK {
     @JvmStatic
     inline fun <reified A : BaseWebKMultiLangActivity> startWebMultiLangActivity(context: Context, strUrl: String) {
         startWebMultiLangActivity<A>(context, "", strUrl)
@@ -19,6 +21,7 @@ object WebKUtil {
 
     @JvmStatic
     inline fun <reified A : BaseWebKMultiLangActivity> startWebMultiLangActivity(context: Context, title: String, strUrl: String) {
+        UtilKLogWrapper.d(TAG, "startWebMultiLangActivity title $title strUrl $strUrl")
         context.startContext<A> {
             putExtra(BaseWebKMultiLangActivity.EXTRA_WEBKIT_BASIC_TITLE, title)
             putExtra(BaseWebKMultiLangActivity.EXTRA_WEBKIT_BASIC_URl, strUrl)
