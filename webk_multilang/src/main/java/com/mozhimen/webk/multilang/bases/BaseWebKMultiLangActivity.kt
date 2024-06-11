677f6672eeb0b97e5418485bf348c9a485d2d914
Kolin Zhao & Mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-06-11 19:58:17 +0800

    0611

--- webk_multilang/src/main/java/com/mozhimen/webk/multilang/bases/BaseWebKMultiLangActivity.kt
+++ webk_multilang/src/main/java/com/mozhimen/webk/multilang/bases/BaseWebKMultiLangActivity.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Bitmap
 import android.os.Bundle
-import android.util.Log
 import com.mozhimen.basick.utilk.android.util.UtilKLogWrapper
 import android.webkit.WebChromeClient
 import android.webkit.WebView
